01cde42bab36312991ae8531ef8c69c774f9c107
nhithai4620 <nguyennhithai4620@gmail.com>
2022-09-13 20:56:42 +0700

    Add jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,109 +1,120 @@
-const {User} = require("../models/user")
-const jwt = require('jsonwebtoken');
+const { User } = require("../models/user");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 let refreshTokens = [];
 
 const authController = {
-    register: async(req, res) =>{
-        
-        try{
-            var personInfo = req.body;
-            const salt = await bcrypt.genSalt(10);
-            const hashed = await bcrypt.hash(req.body.password, salt);
+  register: async (req, res) => {
+    try {
+      var personInfo = req.body;
+      const salt = await bcrypt.genSalt(10);
+      const hashed = await bcrypt.hash(req.body.password, salt);
 
-            const newUser = await new User({
-                email: personInfo.email,
-                password: hashed,
-            })
+      const newUser = await new User({
+        email: personInfo.email,
+        password: hashed,
+      });
 
-            User.findOne({email:personInfo.email},async (err,data)=>{
-                if(!data){
-                    const saveUser = await newUser.save();
-                    res.status(200).json(saveUser);
-                }else{
-                    res.status(500).json({ status: 'error', error: 'Username already in use' });
-                }
-            })
-            
-        }catch(err){
-            console.log(err);
-            res.status(500).json(err);
+      User.findOne({ email: personInfo.email }, async (err, data) => {
+        if (!data) {
+          const saveUser = await newUser.save();
+          res.status(200).json(saveUser);
+        } else {
+          res
+            .status(500)
+            .json({ status: "error", error: "Username already in use" });
         }
-    },
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 
-    login: async(req, res) =>{
-        try {
-            const { username, password } = req.body
-    
-            User.findOne({email:username},async (err,data)=>{
-                const validPassword = await bcrypt.compare(
-                    password,
-                    data.password
-                );
+  login: async (req, res) => {
+    try {
+      const { username, password } = req.body;
 
-                if(data){
+      User.findOne({ email: username }, async (err, data) => {
+        const validPassword = await bcrypt.compare(password, data.password);
 
-                    if(validPassword){
-                        const accessToken = authController.generateAccessToken(data);
-                        const refreshToken = authController.generateRefreshToken(data);
-                        refreshTokens.push(refreshToken);
+        if (data) {
+          if (validPassword) {
+            const accessToken = authController.generateAccessToken(data);
+            const refreshToken = authController.generateRefreshToken(data);
+            refreshTokens.push(refreshToken);
 
-                        res.status(200).json({message: 'success', accessToken: accessToken, refreshToken: refreshToken });
-
-                    }else{
-                        res.status(403).json({ status: 'error', error: 'UnauthorizedException', message: 'Unauthorized' });
-                    }
-                } else{
-                    res.status(403).json({ status: 'error', message: 'This Email Is not regestered!' });
-                }
+            res
+              .status(200)
+              .json({
+                message: "success",
+                accessToken: accessToken,
+                refreshToken: refreshToken,
+              });
+          } else {
+            res
+              .status(403)
+              .json({
+                status: "error",
+                error: "UnauthorizedException",
+                message: "Unauthorized",
+              });
+          }
+        } else {
+          res
+            .status(403)
+            .json({
+              status: "error",
+              message: "This Email Is not regestered!",
             });
-        } catch (error) {
-            console.log(err);
-            res.status(500).json(err);
         }
-    },
+      });
+    } catch (error) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 
-    generateAccessToken: (user) => {
-        return jwt.sign(
-          {
-            id: user._id,
-            username: user.username
-          },
-          process.env.JWT_SECRET,
-          { expiresIn: "30s" }
-        );
-    },
+  generateAccessToken: (user) => {
+    return jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30s" }
+    );
+  },
 
-    generateRefreshToken: (user) => {
-        return jwt.sign(
-          {
-            id: user._id,
-            username: user.username
-          },
-          process.env.JWT_REFRESH_KEY,
-          { expiresIn: "365d" }
-        );
-    },
+  generateRefreshToken: (user) => {
+    return jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+      },
+      process.env.JWT_REFRESH_KEY,
+      { expiresIn: "365d" }
+    );
+  },
 
-    profile: async(req, res) =>{
-        const bearerHeader = req.headers['authorization'];
-        try {
-            jwt.verify(bearerHeader,JWT_SECRET,(err,data)=>{
-                if(err)
-                    res.sendStatus(403);
-                else{
-                    res.json({
-                        message:`Welcome to profile`,
-                        userData: data
-                    })   
-                }
-            })
-        } catch (error) {
-            console.log(err);
-            res.status(500).json(err);
+  profile: async (req, res) => {
+    const bearerHeader = req.headers["authorization"];
+    try {
+      jwt.verify(bearerHeader, JWT_SECRET, (err, data) => {
+        if (err) res.sendStatus(403);
+        else {
+          res.json({
+            message: `Welcome to profile`,
+            userData: data,
+          });
         }
-    },
+      });
+    } catch (error) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 };
 
 module.exports = authController;
